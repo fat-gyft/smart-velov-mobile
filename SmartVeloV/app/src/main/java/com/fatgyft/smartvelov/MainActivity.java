2001fe92ed915efb953642b0f72e0da13afb5155
Felipe Matias <felipe.matias371@gmail.com>
2015-04-27 22:54:16 +0200

    map display working

--- SmartVeloV/app/src/main/java/com/fatgyft/smartvelov/MainActivity.java
+++ SmartVeloV/app/src/main/java/com/fatgyft/smartvelov/MainActivity.java
@@ -1,17 +1,45 @@
 package com.fatgyft.smartvelov;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends ActionBarActivity  implements LocationListener {
+
+    private MapView mapView;
+    private IMapController mapController;
+
+    private LocationManager locationManager;
+    private GeoPoint currentLocation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapView = (MapView) this.findViewById(R.id.mapview);
+        mapView.setTileSource(TileSourceFactory.MAPQUESTOSM);
+        mapView.setBuiltInZoomControls(true);
+        mapView.setMultiTouchControls(true);
+        mapController = this.mapView.getController();
+        mapController.setCenter(new GeoPoint(45.7527,4.8494));
+        mapController.setZoom(12);
+
+
+
     }
 
 
@@ -35,5 +63,33 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location)
+    {
+        Toast.makeText(this,
+                "latitude = " + location.getLatitude() * 1e6 + " longitude = " + location.getLongitude() * 1e6,
+                Toast.LENGTH_SHORT).show();
+
+        int latitude = (int) (location.getLatitude() * 1E6);
+        int longitude = (int) (location.getLongitude() * 1E6);
+        GeoPoint point = new GeoPoint(latitude, longitude);
+        mapController.setCenter(point);
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
