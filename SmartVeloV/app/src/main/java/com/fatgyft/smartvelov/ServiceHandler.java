79e80028ea6306f7ce4befe971cf21a1e612d0b3
Titouan <titouan.thibaud@gmail.com>
2015-04-30 10:22:12 +0200

    Added Bluetooth icons

---
+++ SmartVeloV/app/src/main/java/com/fatgyft/smartvelov/ServiceHandler.java
@@ -0,0 +1,125 @@
+package com.fatgyft.smartvelov;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+
+public class ServiceHandler {
+
+    static String response = null;
+    public final static int GET = 1;
+    public final static int POST = 2;
+
+    public ServiceHandler() {
+    }
+
+    /**
+     * Making service call
+     *
+     * @url - url to make request
+     * @method - http request method
+     */
+    public String makeServiceCall(String url, int method) {
+        return this.makeServiceCall(url, method, null);
+    }
+
+    /**
+     * Making service call
+     *
+     * @url - url to make request
+     * @method - http request method
+     * @params - http request params
+     */
+    public String makeServiceCall(String url, int method, List<NameValuePair> params) {
+        try {
+            // http client
+            HttpParams httpParameters = new BasicHttpParams();
+            HttpProtocolParams.setContentCharset(httpParameters, HTTP.UTF_8);
+            HttpProtocolParams.setHttpElementCharset(httpParameters, HTTP.UTF_8);
+
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            httpClient.getParams().setParameter(CoreProtocolPNames.USER_AGENT, "AndroidApp");
+            HttpEntity httpEntity = null;
+            HttpResponse httpResponse = null;
+
+            // Checking http request method type
+            if (method == POST) {
+
+                HttpPost httpPost = new HttpPost(url);
+                // adding post params
+                if (params != null) {
+                    UrlEncodedFormEntity e = new UrlEncodedFormEntity(params, HTTP.UTF_8);
+                    httpPost.setEntity(e);
+                }
+
+                httpResponse = httpClient.execute(httpPost);
+
+            } else if (method == GET) {
+                // appending params to url
+                if (params != null) {
+                    String paramString = URLEncodedUtils
+                            .format(params, "utf-8");
+                    url += "?" + paramString;
+                }
+                HttpGet httpGet = new HttpGet(url);
+
+                httpResponse = httpClient.execute(httpGet);
+
+            }
+            httpEntity = httpResponse.getEntity();
+            response = EntityUtils.toString(httpEntity);
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return response;
+
+    }
+
+    /**
+     * Static method used to check if the device is connected to internet
+     *
+     * @param context
+     * @return connexion status
+     */
+    public static boolean isConnected(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null) {
+            NetworkInfo.State networkState = networkInfo.getState();
+            if (networkState.compareTo(NetworkInfo.State.CONNECTED) == 0)
+                return true;
+        }
+        return false;
+    }
+
+
+}
+
